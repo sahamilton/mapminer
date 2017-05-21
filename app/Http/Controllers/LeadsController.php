c73519a630f05bdf8c4a052f107b4e8eeb7f6654
sahamilton <stephen@crescentcreative.com>
2017-05-21 11:10:31 -0700

    Adding lead address search

--- app/Http/Controllers/LeadsController.php
+++ app/Http/Controllers/LeadsController.php
@@ -29,7 +29,7 @@ class LeadsController extends BaseController
 				dd('bummer');
 				
 			}
-            dd($geoCode[0]->getLatitude());
+            dd($geoCode->first()->getLatitude());
 			$people = $this->person->findNearByPeople($geoCode[0]['latitude'],$geoCode[0]['longitude'],$request->get('distance'),5,'Sales');
 			return response()->view('leads.address',compact('people'));
 			
