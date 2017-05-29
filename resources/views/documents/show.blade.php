7de6e1e98413f656e335da6c9c272fee562f1f24
sahamilton <stephen@crescentcreative.com>
2017-05-29 16:20:21 -0700

    Fixing show document

--- resources/views/documents/show.blade.php
+++ resources/views/documents/show.blade.php
@@ -7,13 +7,14 @@
 <p><strong>Available Until:</strong> {{$document->dateto->format('Y-m-d')}}</p>
 <p><strong>Description:</strong> {{$document->description}}</p>
 <p><strong>Summary:</strong> {{$document->summary}}</p>
-<p><strong>Type:</strong></p>
-<p><strong>location:</strong> <a href="{{$document->location}}" target="_blank" >{{$document->location}}</a></p>
+<p><strong>Type:{{$document->doctype}}</strong></p>
+<p><strong>Location:</strong> <a href="{{$document->location}}" target="_blank" >{{$document->location}}</a></p>
 
-@if($document->rank && count($document->rank) > 0 && count($document->score)> 0 && count($document->rankings) >0)
+@if(isset($document->rank) && count($document->rank) > 0 && count($document->score)> 0 && count($document->rankings) >0)
    <?php $rank = round($document->rank[0]->rank,2);?>
 
-@endif
- <div id="{{$document->id}}" data-rating="{{intval($rank)}}" class="starrr" ></div>
+
+ <div id="{{$document->id}}" data-rating="{{intval(isset($rank)}}" class="starrr" ></div>
             <span id="count-existing">{{$rank}}</span>
+@endif
 @endsection
