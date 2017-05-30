bfed2adf04d4459ea27ad29345e80ea1648a8626
Stephen Hamilton <stephen@crescentcreative.com>
2017-05-29 17:49:15 -0700

    Fixing document show

--- resources/views/documents/show.blade.php
+++ resources/views/documents/show.blade.php
@@ -1,5 +1,6 @@
 @extends ('site.layouts.default')
 @section('content')
+
 <h1>{{$document->title}}</h1>
 <p><strong>Editor:</strong> {{$document->author->person->fullName()}}</p>
 <p><strong>Published:</strong> {{$document->created_at->format('Y-m-d')}}</p>
@@ -8,10 +9,10 @@
 <p><strong>Description:</strong> {{$document->description}}</p>
 <p><strong>Summary:</strong> {{$document->summary}}</p>
 <p><strong>Type:{{$document->doctype}}</strong></p>
-<p><strong>Location:</strong> <a href="{{$document->location}}" target="_blank" >{{$document->location}}</a></p>
+<p><strong>Location:</strong> <a href="{{$document->location}}" target="_blank" >{{$document->doctype}}</a></p>
 
 @if(isset($document->rank) && count($document->rank) > 0 && count($document->score)> 0 && count($document->rankings) >0)
-   <?php $rank = round($document->rank[0]->rank,2);?>
+   <?php $rank = round($document->rank[0]->rank,2));?>
 
 
  <div id="{{$document->id}}" data-rating="{{intval(isset($rank)}}" class="starrr" ></div>
