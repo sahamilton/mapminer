3a1310a63a1a7f42ac5b3fcf292a34cded82c1cc
Stephen Hamilton <stephen@crescentcreative.com>
2017-06-06 18:23:26 -0700

    Fixing sudso only in local

--- resources/views/site/layouts/maps.blade.php
+++ resources/views/site/layouts/maps.blade.php
@@ -91,7 +91,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 			<!-- ./ content -->
 		</div>
 		<!-- ./ container -->
-@if (config('app.debug'))
+ @if (config('app.debug') && auth()->check() && config('app.env')=='local')
     @include('sudosu::user-selector')
 @endif
 		<!-- the following div is needed to make a sticky footer -->
