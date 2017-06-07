71a912d1fe00095296ed6e39b8d0e1ad12f9098a
Stephen Hamilton <stephen@crescentcreative.com>
2017-06-06 18:34:40 -0700

    Working on admin middleware

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -16,7 +16,7 @@ class AdminMiddleware
     public function handle($request, Closure $next)
     {
 
-        if (! auth()-user()->hasRole('Admin')) 
+        if (! auth()->user()->hasRole('Admin')) 
         {
             return redirect('home');
         }
