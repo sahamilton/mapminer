9ae25578d269eba234c574c93e530a6904471bec
Stephen Hamilton <stephen@crescentcreative.com>
2017-06-06 19:07:59 -0700

    Fixing calendar

--- resources/views/salesactivity/partials/_form.blade.php
+++ resources/views/salesactivity/partials/_form.blade.php
@@ -18,8 +18,8 @@
 <div id="datepicker" class="form-group @if ($errors->has('datefrom')) has-error @endif">
 <label class="control-label col-sm-2" for="datefrom">Date From:</label>
    <div class="input-group date col-sm-4">       
-  <input type="text"  required name='datefrom' class="form-control" readonly value="{{isset($activity->datefrom) ? date('m/d/Y',strtotime( 
-  $activity->datefrom)) : date('m/d/Y')}}" />
+  <input type="text"  required name='datefrom' class="form-control"  value="{{isset($activity->datefrom) ? 
+  $activity->datefrom->format('m/d/Y') : date('m/d/Y')}}" />
   <span class="input-group-addon"><i class="glyphicon glyphicon-th"></i></span>
  </div> 
  @if ($errors->has('datefrom')) <p class="help-block">{{ $errors->first('datefrom') }}</p> @endif
@@ -29,8 +29,7 @@
 <div id="datepicker1" class="form-group @if ($errors->has('dateto')) has-error @endif">
 <label class="control-label col-sm-2" for="dateto">DateTo:</label>
           <div class="input-group date col-sm-4">
-  <input type="text"  required name ='dateto' class="form-control" readonly value="{{isset($activity->dateto) ? date('m/d/Y',strtotime( 
-  $activity->dateto)): date('m/d/Y', strtotime("+1 months",strtotime(date('m/d/Y'))))}}" />
+  <input type="text"  required name ='dateto' class="form-control"  value="{{isset($activity->dateto) ? $activity->dateto->format('m/d/Y') : date('m/d/Y', strtotime("+1 months",strtotime(date('m/d/Y'))))}}" />
   <span class="input-group-addon"><i class="glyphicon glyphicon-th"></i></span>
 </div>   
 @if ($errors->has('dateto')) <p class="help-block">{{ $errors->first('dateto') }}</p> @endif
