47c5d27dff16962960613ec206a98520cc48b841
sahamilton <stephen@crescentcreative.com>
2017-06-29 12:53:11 -0700

    Removing profile footer from production

--- resources/views/site/layouts/footer.blade.php
+++ resources/views/site/layouts/footer.blade.php
@@ -3,10 +3,10 @@
 		<div class="muted credit col-sm-6 " style="color:#000000">
 		&copy;2014 - <?php echo date("Y");?>  ELA Consulting Group, LLC / TrueBlue, Inc.
 		</div>
-		 
+		 @if(config('app.env')=='local' or config('app.env')=='staging'))
 			<div class="pull-right col-sm-4" style="color:black">
 				{{{App::environment()}}} | {{{App::version()}}}| {{{config('mapminer.app_version')}}} | {{ phpversion() }}
 			</div>
-		
+		@endif
 	</div>
 </div>
