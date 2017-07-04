ec4180516269e236c5b3bd1373eb93e48f46edf6
sahamilton <stephen@crescentcreative.com>
2017-07-03 17:05:26 -0700

    Working on load data file mysql

--- config/database.php
+++ config/database.php
@@ -51,6 +51,7 @@ return [
             'prefix' => '',
             'strict' => false,
             'engine' => null,
+            'options'    => [PDO::MYSQL_ATTR_LOCAL_INFILE=>true],
         ],
 
         'pgsql' => [
