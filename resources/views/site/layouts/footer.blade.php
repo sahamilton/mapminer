ede55645bf6cfd17925aef804053192c8e013273
sahamilton <stephen@crescentcreative.com>
2017-07-06 09:54:03 -0700

    Fixing footer

--- resources/views/site/layouts/footer.blade.php
+++ resources/views/site/layouts/footer.blade.php
@@ -1,11 +1,12 @@
 <div id="footer" >
 	<div class="container">
 		<div class="muted credit col-sm-6 " style="color:#000000">
-		&copy;2014 - <?php echo date("Y");?>  ELA Consulting Group, LLC / TrueBlue, Inc.
+		&copy;2014 - <?php echo date("Y");?>  <a href="http://www.elaconsultinggroup.com"
+		title="Visit the ELA COnsulting Group website" target="_blank">ELA Consulting Group, LLC </a>/ TrueBlue, Inc.
 		</div>
-		 @if(config('app.env')=='local' or config('app.env')=='staging'))
+		 @if(config('app.env')=='local' or config('app.env')=='staging')
 			<div class="pull-right col-sm-4" style="color:black">
-				{{{App::environment()}}} | {{{App::version()}}}| {{{config('mapminer.app_version')}}} | {{ phpversion() }}
+				{{App::environment()}} | {{App::version()}}| {{config('mapminer.app_version')}} | {{ phpversion() }}
 			</div>
 		@endif
 	</div>
