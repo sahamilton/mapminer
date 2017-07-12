1f7db6e8a1e7baf0e608668059a28f0a5f5663ec
sahamilton <stephen@crescentcreative.com>
2017-07-12 12:50:20 -0700

    Working on formatting full text search box

--- resources/views/salesprocess/index.blade.php
+++ resources/views/salesprocess/index.blade.php
@@ -1,79 +1,74 @@
 @extends ('admin.layouts.default')
 @section('content')
 <div class="container">
-<h2>Sales Process</h2>
-<div class="container">
-
-<div class="pull-right">
-        <a href ="{{route('process.create')}}"><button class="btn btn-success" >Add Sales Step
-        </button></a>
+    <h2>Sales Process</h2>
+    <div class="pull-right">
+        <a href ="{{route('process.create')}}">
+            <button class="btn btn-success" >
+            Add Sales Step
+            </button>
+        </a>
     </div>    
-   
-        
-<div class="col-md-10 col-md-offset-1">
+
+
+    <div class="col-md-10 col-md-offset-1">
         <table class="table" id = "sorttable">
             <thead>
 
                 <th>Name</th>
                 <th>Sequence</th>
-                
+
                 <th>Actions</th>
-                
+
             </thead>
             <tbody>
-            @foreach ($process as $step)
+                @foreach ($process as $step)
 
                 <tr> 
-                
-                <td>{{$step->step }}</td>
-               
-                <td>
-                        {{$step->sequence}}
-                </td>
-                
-                 <td class="col-md-2">
-              
 
-                <div class="btn-group">
-                    <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown">
-                    <span class="caret"></span>
-                    <span class="sr-only">Toggle Dropdown</span>
-                    </button>
-                    <ul class="dropdown-menu" role="menu">
+                    <td>{{$step->step }}</td>
 
-                    <li><a href="{{route('process.edit',$step->id)}}"><i class="fa fa-pencil" aria-hidden="true"> </i>Edit Sales Step</a></li>
+                    <td>{{$step->sequence}}</td>
 
-                    <li><a data-href="{{route('process.destroy',$step->id)}}" 
-                    data-toggle="modal" 
-                    data-target="#confirm-delete" 
-                    data-title = "location" 
-                    href="#"><i class="fa fa-trash-o" aria-hidden="true"> </i> Delete Sales Step</a>
-                    </li>
+                    <td class="col-md-2">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown">
+                                <span class="caret"></span>
+                                <span class="sr-only">Toggle Dropdown</span>
+                            </button>
+                            <ul class="dropdown-menu" role="menu">
 
+                                <li>
+                                <a href="{{route('process.edit',$step->id)}}">
+                                <i class="fa fa-pencil" aria-hidden="true"> </i>
+                                Edit Sales Step</a>
+                                </li>
 
+                                <li>
+                                <a data-href="{{route('process.destroy',$step->id)}}" 
+                                    data-toggle="modal" 
+                                    data-target="#confirm-delete" 
+                                    data-title = "location" 
+                                    href="#">
+                                    <i class="fa fa-trash-o" aria-hidden="true"> </i> 
+                                    Delete Sales Step
+                                </a>
+                                </li>
+                            </ul>
+                        </div>
 
-                    </ul>
-                </div>
-               
-               </td> 
-
-
-                            
-                  
-
-             
-                
-               
+                    </td>
                 </tr>  
-            
-            @endforeach
+
+                @endforeach
             </tbody>
-            
+
 
 
         </table>
-        </div>
     </div>
+
 </div>
-  @include('partials._modal')
+
+@include('partials._modal')
 @endsection
