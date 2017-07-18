c5888c8067d7422a30a0f57e2011a7b996df870c
sahamilton <stephen@crescentcreative.com>
2017-07-17 18:14:33 -0700

    Debugging email

--- app/Mail/SendEmail.php
+++ app/Mail/SendEmail.php
@@ -36,7 +36,7 @@ class SendEmail extends Mailable
         
         return $this->markdown('emails.email')->subject($this->data->subject)
         ->from('info@tbmapminer.com','MapMiner')
-        ->to($this->recipient->userdetails->email, $this->recipient->fullName())
-        ->cc(auth()->user()->email,auth()->user()->person->fullName());
+        ->to($this->recipient->userdetails->email, $this->recipient->postName())
+        ->cc(auth()->user()->email, auth()->user()->person->postName());
     }
 }
