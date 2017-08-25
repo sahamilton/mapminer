5542cd1e7da048bbd032878e00a55c007460d301
sahamilton <stephen@crescentcreative.com>
2017-08-25 07:20:26 -0700

    Fixing watch list notes

--- app/Watch.php
+++ app/Watch.php
@@ -22,7 +22,7 @@ class Watch extends Model {
 	
 	public function watchnotes() 
 	{
-		return $this->hasMany(Note::class,'location_id','location_id');	
+		return $this->hasMany(Note::class,'related_id','location_id');	
 	}
 	
 	public function exportWatchList($fields,$watchList){
