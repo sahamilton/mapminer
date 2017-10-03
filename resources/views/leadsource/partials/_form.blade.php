8647ff50117b809bfa5ec677354f6debffd9e186
sahamilton <stephen@crescentcreative.com>
2017-10-03 08:46:55 -0700

    Reformatting prospect source form

--- resources/views/leadsource/partials/_form.blade.php
+++ resources/views/leadsource/partials/_form.blade.php
@@ -1,7 +1,7 @@
 <!-- source -->
 <div class="form-group{{ $errors->has('source') ? ' has-error' : '' }}">
-    <label class="col-md-4 control-label">Source Name</label>
-    <div class="col-md-6">
+    <label class="col-md-2 control-label">Source Name</label>
+    <div class="input-group input-group-lg">
         <input required type="text" class="form-control" name='source' description="source" value="{{ old('source', isset($leadsource) ? $leadsource->source : '' )}}" placeholder="source">
         <span class="help-block">
             <strong>{{ $errors->has('source') ? $errors->first('source') : ''}}</strong>
@@ -10,8 +10,8 @@
 </div>
 <!-- Description -->
 <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }}">
-    <label class="col-md-4 control-label">Description</label>
-    <div class="col-md-6">
+    <label class="col-md-2 control-label">Description</label>
+    <div class="input-group input-group-lg">
         <textarea required class="form-control" name='description' title="description">{{ old('description', isset($leadsource) ? $leadsource->description : '')}}</textarea>
 
         <span class="help-block">
@@ -22,8 +22,8 @@
 </div> 
 <!-- Reference -->
 <div class="form-group{{ $errors->has('reference') ? ' has-error' : '' }}">
-    <label class="col-md-4 control-label">Reference</label>
-    <div class="col-md-6">
+    <label class="col-md-2 control-label">Reference</label>
+    <div class="input-group input-group-lg">
         <input type="text" class="form-control" name='reference' description="reference" value="{{ old('reference', isset($leadsource) ? $leadsource->reference : "")}}" placeholder="reference">
         <span class="help-block">
             <strong>{{ $errors->has('reference') ? $errors->first('reference') : ''}}</strong>
@@ -31,9 +31,11 @@
     </div>
 </div>
 <!-- Dates from / to -->
-<legend>Available From / To</legend>
+<div class="form-group">
+<label>Available From / To</label>
+</div>
     <div class="form-group{{ $errors->has('from)') ? ' has-error' : '' }}">
-        <label class="col-md-4 control-label" for="datefrom">Available From</label>
+        <label class="col-md-2 control-label" for="datefrom">Available From</label>
         <div class="input-group input-group-lg">
             <input required class="form-control" type="text" name="datefrom"  id="fromdatepicker" 
             value="{{  old('datefrom', isset($leadsource) ? $leadsource->datefrom->format('m/d/Y'): date('m/d/Y')) }}"/>
@@ -45,7 +47,7 @@
     </div>
 
     <div class="form-group{{ $errors->has('dateto') ? ' has-error' : '' }}">
-        <label class="col-md-4 control-label" for="dateto">Available To</label>
+        <label class="col-md-2 control-label" for="dateto">Available To</label>
         <div class="input-group input-group-lg ">
             <input required class="form-control" type="text" name="dateto"  id="todatepicker" 
             value="{{  old('dateto', isset($leadsource) ?  $leadsource->dateto->format('m/d/Y') : date('m/d/Y',strtotime('+3 months'))) }}"/>
@@ -57,7 +59,7 @@
     </div>
     <legend>Industry Verticals</legend>
     <div class="form-group{{ $errors->has('vertical') ? ' has-error' : '' }}">
-        <label class="col-md-4 control-label" for="vertical">Industry Verticals</label>
+        <label class="col-md-2 control-label" for="vertical">Industry Verticals</label>
         <div class="input-group input-group-lg ">
 @include('leadsource.partials._verticals')  
 <span class="help-block{{ $errors->has('vertical') ? ' has-error' : '' }}">
