e217c900884c65ec5cc14268aaf73402f1965b5e
sahamilton <stephen@crescentcreative.com>
2017-10-05 11:09:59 -0700

    Working on admin prospect summary

--- resources/views/leads/person.blade.php
+++ resources/views/leads/person.blade.php
@@ -18,7 +18,7 @@
   <li class="active"><a data-toggle="tab" href="#map"><strong>Map View</strong></a></li>
  
   <li><a data-toggle="tab" href="#leads"><strong>Prospects</strong></a></li>
-  <li><a data-toggle="tab" href="#stats"><strong>Stats</strong></a></li>
+
 
   
 
@@ -31,9 +31,7 @@
 <div id="leads" class="tab-pane fade in ">
 @include('leads.partials._tabpersonsleads')
 </div>
-<div id="stats" class="tab-pane fade in ">
 
-</div>
 </div>
 @include('partials._scripts')
 @endsection
