35e77c0dc503ed8b5e8d52793f5a08a288ec7b24
Stephen Hamilton <stephen@crescentcreative.com>
2018-01-27 12:41:16 -0800

    Working on closets reps to locations

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -246,11 +246,7 @@ class CompaniesController extends BaseController {
 		// get company locations
 		
 		$locations = $this->getCompanyLocations($id,$segment,$company);
-		foreach ($locations as $location){
-			$team[$location->id]=$location->nearbySalesRep()->get();
-			
-			//$salesrep[$locations->id=>$location->nearbySalesRep()->pluck('fistname'.' '.'lastname','id')->toArray());
-		}
+		
 		
 		$states = $this->getStatesInArray($locations);
 		$segments = $this->getCompanySegments($company);
@@ -285,6 +281,11 @@ class CompaniesController extends BaseController {
 
 		$data['type']='company';
 		$mywatchlist = $this->locations->getWatchList();
+		foreach ($locations as $location){
+			$team[$location->id]=$location->nearbySalesRep()->get();
+			
+			//$salesrep[$locations->id=>$location->nearbySalesRep()->pluck('fistname'.' '.'lastname','id')->toArray());
+		}
 		return response()->view('companies.show', compact('data','company','locations','count','limited','mywatchlist','states','filtered','filters','segments','team'));
 	}
 
