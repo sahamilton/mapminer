317a28cdc92dedbabcb865b36d8efa1df94065ac
sahamilton <stephen@crescentcreative.com>
2018-03-20 08:47:30 -0700

    Fixing sudosu for staging

--- resources/views/site/layouts/default.blade.php
+++ resources/views/site/layouts/default.blade.php
@@ -85,7 +85,7 @@
 
 
 
-@if (config('app.debug') && auth()->check() && config('app.env')!='production' )
+@if (config('app.debug') && auth()->check() && config('app.env')=='local' )
     @include('sudosu::user-selector')
 @endif
 		<!-- the following div is needed to make a sticky footer -->
