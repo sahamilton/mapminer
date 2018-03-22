35f875822dc35dea5998ad527c4ea4ea57672758
Stephen Hamilton <stephen@crescentcreative.com>
2018-03-22 09:15:24 -0700

    Workingon git version

--- app/GitVersion.php
+++ app/GitVersion.php
@@ -14,11 +14,12 @@ class GitVersion extends Model
     public static function get()
     {
         $commitHash = trim(exec('git log --pretty="%h" -n1 HEAD'));
+        $branch = exec('git log -n1 -s --pretty=%d HEAD');
 
         $commitDate = new \DateTime(trim(exec('git log -n1 --pretty=%ci HEAD')));
         $commitDate->setTimezone(new \DateTimeZone('America/Los_Angeles'));
 
-        return sprintf('v%s.%s.%s-maj.%s (%s)', self::MAJOR, self::MINOR, self::PATCH, $commitHash, $commitDate->format('Y-m-d H:m:s'));
+        return sprintf('v%s.%s.%s.%s (%s)', self::MAJOR, self::MINOR, self::PATCH,$branch, $commitHash, $commitDate->format('Y-m-d H:m:s'));
     }
 
 }
