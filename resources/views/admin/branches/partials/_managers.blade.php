55ee739efdac04ddbe3f0f670c8d716d263fc85d
sahamilton <stephen@crescentcreative.com>
2018-05-31 11:01:24 -0700

    Adding Branch Management

--- resources/views/admin/branches/partials/_managers.blade.php
+++ resources/views/admin/branches/partials/_managers.blade.php
@@ -1,17 +1,30 @@
 <table id ='sorttable' class='table table-striped table-bordered table-condensed table-hover'>
 <thead>
 	<th>Manager</th>
+	<th>Serviceline</th>
 	<th>Employee Id</th>
 	<th>Email</th>
 	<th>Reports To</th>
+	
 </thead>
 	<tbody>
 		@foreach ($people as $manager)
 		<tr>
 			<td><a href="{{route('person.details',$manager->id)}}">{{$manager->fullName()}}</a></td>
-			<td>{{$manager->userdetails->employee_id}}</td>
+			<td>
+				@if(count($manager->userdetails->serviceline)>0)
+					<ul style=" list-style-type: none;">
+					@foreach ( $manager->userdetails->serviceline as $serviceline)
+						<li>{{$serviceline->ServiceLine}}</li>
+					@endforeach
+					</ul>
+
+				@endif
+		</td>
+		<td>{{$manager->userdetails->employee_id}}</td>
 			<td>{{$manager->userdetails->email}}</td>
 			<td>@if(count($manager->reportsTo)>0){{$manager->reportsTo->fullName()}}@endif</td>
+			
 		</tr>
 		@endforeach
 	</tbody>
