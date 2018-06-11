6c1fbada9767856059e8cf944d48e572824e6bc7
sahamilton <stephen@crescentcreative.com>
2018-06-11 12:52:00 -0700

    Updating the person rules validation

--- app/Http/Requests/UserFormRequest.php
+++ app/Http/Requests/UserFormRequest.php
@@ -25,18 +25,35 @@ class UserFormRequest extends FormRequest
      */
     public function rules(Request $request)
     {
-       
-       
-        return [
-        'username'=>'required|alpha_num|unique:users,username,' . $request->segment(3),
-        'firstname'=>'required',
-        'lastname'=>'required',
-        'email' => 'required|email|unique:users,email,' . $request->segment(3),
-        'employee_id' => 'required|unique:users,employee_id,' . $request->segment(3),
-        'password'=>'confirmed',
-        'serviceline'=>'required',
-        'employee_id'=>'required|unique:users,employee_id,' . $request->segment(3),
+        $salesrules=array();
+
+       if (count(array_intersect($request->get('roles'), [5,6,7,8]))>0){
+            $salesrules = ['reports_to'=>'required'];
+        }
+       if (count(array_intersect($request->get('roles'), [9]))>0){
+            
+            $branchrules = ['branches'=>'required_without:branchstring',
+                        'branchstring'=>'required_without:branches',];
+        }
+        $rules = [
+            'username'=>'required|alpha_num|unique:users,username,' . $request->segment(3),
+            'firstname'=>'required',
+            'lastname'=>'required',
+            'email' => 'required|email|unique:users,email,' . $request->segment(3),
+            'employee_id' => 'required|unique:users,employee_id,' . $request->segment(3),
+            'password'=>'confirmed',
+            'serviceline'=>'required',
 
         ];
+        return array_merge($salesrules,$branchrules,$rules) ;
     }
+    public function messages()
+    {
+        return [
+            'reports_to.required' => 'Reports to is required for sales roles',
+            'branches.required_without' =>'Branches or Branchstring required for branchmanagers',
+            'branchstring.required_without' =>'Branches or Branchstring required for branchmanagers',
+        ];
+    }
+
 }
