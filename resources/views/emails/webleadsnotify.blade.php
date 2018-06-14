d8421ccface5b123ca5da744555539b1c96f46f0
Stephen Hamilton <stephen@crescentcreative.com>
2018-06-13 19:38:23 -0700

    Adding NotifyWebLeads

---
+++ resources/views/emails/webleadsnotify.blade.php
@@ -0,0 +1,19 @@
+@component('mail::message')
+
+## New Prospects 
+
+{{$team->firstname}}, 
+
+{!! $data['message'] !!}
+
+@component('mail::button', ['url' => route('salesleads.index'), 'color' => 'blue'])
+        Check out your {{$data['source']->title}} sales prospects and resources.
+@endcomponent
+
+<em> If you’re having trouble clicking the  button, copy and paste the URL below
+into your web browser: [{{ route('salesleads.index')}}]({{ route('salesleads.index')}}) </em>
+
+Sincerely
+        
+{{env('APP_NAME')}}
+@endcomponent
