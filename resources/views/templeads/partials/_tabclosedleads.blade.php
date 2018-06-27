8f27ab1291ad7a2e37a0c8e18abd5a49649d4637
sahamilton <stephen@crescentcreative.com>
2018-06-27 10:48:16 -0700

    Working on simplification of leads

--- resources/views/templeads/partials/_tabclosedleads.blade.php
+++ resources/views/templeads/partials/_tabclosedleads.blade.php
@@ -1,36 +1,36 @@
 <table class="table" id = "sorttable">
-            <thead>
+    <thead>
 
-                <th>Company</th>
-                <th>Address</th>
-                <th>City</th>
-                <th>State</th>
-                <th>Notes</th>
-                <th>Ranking</th>
+        <th>Company</th>
+        <th>Address</th>
+        <th>City</th>
+        <th>State</th>
+        <th>Notes</th>
+        <th>Ranking</th>
 
-            </thead>
-            <tbody>
-                @foreach ($closedleads as $lead)
-                  
-                <tr> 
-                    <td><a href="{{route('salesrep.newleads.show',$lead->id)}}">{{$lead->companyname}}</a></td>
-                    <td>{{$lead->address}}</td>
-                    <td>{{$lead->city}}</td>
-                    <td>{{$lead->state}}</td>
-                    <td>
-                        @foreach ($lead->relatedNotes as $note)
-                            {{$note->note}}<hr />
-                        @endforeach
-                    </td>
-                    <td>
-                       {{$lead->closedleads->first()->pivot->rating}}
-                       <span data-rating="{{$lead->salesteam->first()->pivot->rating}}" class="starrr" style="color:#E77C22"></span>
-                    </td>
-                </tr>  
-
+    </thead>
+    <tbody>
+        @foreach ($closedleads as $lead)
+          
+        <tr> 
+            <td><a href="{{route('salesrep.newleads.show',$lead->id)}}">{{$lead->companyname}}</a></td>
+            <td>{{$lead->address}}</td>
+            <td>{{$lead->city}}</td>
+            <td>{{$lead->state}}</td>
+            <td>
+                @foreach ($lead->relatedNotes as $note)
+                    {{$note->note}}<hr />
                 @endforeach
-            </tbody>
+            </td>
+            <td>
+               {{$lead->closedleads->first()->pivot->rating}}
+               <span data-rating="{{$lead->salesteam->first()->pivot->rating}}" class="starrr" style="color:#E77C22"></span>
+            </td>
+        </tr>  
+
+        @endforeach
+    </tbody>
 
 
 
-        </table>
+</table>
