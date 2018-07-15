2ef6b5234b8ba5a8668a73fa4852284d7e8bf8a9
sahamilton <stephen@crescentcreative.com>
2018-07-15 11:38:30 -0700

    Fixing issue with lead map jscript

--- resources/views/webleads/partials/map.blade.php
+++ resources/views/webleads/partials/map.blade.php
@@ -1,6 +1,7 @@
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key={{config('maps.api_key')}}"></script>
 
 <script type="text/javascript">
+
 function initialize(){
   var myLatlng = new google.maps.LatLng({{ $lead->lat }},{{ $lead->lng }});
   var mapOptions = {
