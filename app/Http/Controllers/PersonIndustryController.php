9348fd4e39af863a0f375c4325d689113cf493f9
Stephen Hamilton <Stephen@CrescentCreative.com>
2018-10-01 14:13:02 -0700

    Adding Industry focus to profile

---
+++ app/Http/Controllers/PersonIndustryController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Searchfilter;
+use App\PersonIndustry;
+use App\Person;
+use Illuminate\Http\Request;
+
+class PersonIndustryController extends Controller
+{
+    
+    protected $person;
+    protected $industry;
+    protected $searchfilter;
+
+    public function __construct(PersonIndustry $industry, Searchfilter $searchfilter, Person $person){
+        $this->industry = $industry;
+        $this->searchfilter = $searchfilter;
+        $this->person = $person;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $industries = $this->industry->with('industry')->where('person_id','=',auth()->user()->person->id)->get()->pluck('industry.id')->toArray();
+
+        $filters = $this->searchfilter->where('filter','=','Industry & Segments')->first()->getDescendantsAndSelf();
+        return response()->view('industries.index',compact('industries','filters'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $person = $this->person->findOrFail(request('id'));
+        $person->industryfocus()->sync(request('vertical'));
+        return redirect()->route('user.show',auth()->user()->id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PersonIndustry  $personIndustry
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PersonIndustry $personIndustry)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PersonIndustry  $personIndustry
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PersonIndustry $personIndustry)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PersonIndustry  $personIndustry
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PersonIndustry  $personIndustry
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PersonIndustry $personIndustry)
+    {
+        //
+    }
+}
---
+++ resources/views/industries/index.blade.php
@@ -0,0 +1,9 @@
+@extends ('site.layouts.default')
+@section('content')
+<div class="container">
+<h4>Industry Focus</h4>
+@include('industries.partials._verticalform')
+
+</div>
+
+@endsection
