6f8d8266efa5445ca311daa12385dca6c3b6feae
Stephen Hamilton <Stephen@CrescentCreative.com>
2018-12-08 15:14:43 -0800

    Adding besnappy partial

---
+++ resources/views/partials/besnappy.blade.php
@@ -0,0 +1,7 @@
+<script
+    src="//d2s6cp23z9c3gz.cloudfront.net/js/embed.widget.min.js"
+    data-domain="trueblue.besnappy.com"
+    data-lang="en"
+	data-name="{{ auth()->user() ? auth()->user()->person->firstname : ''  }}"  
+	data-email="{{ auth()->user() ? auth()->user()->email : '' }}"  >
+</script>
