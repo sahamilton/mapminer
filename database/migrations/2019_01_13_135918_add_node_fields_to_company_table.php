709ba0c3e6bc0b34c860c7f9a0c1faa163eeb11f
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-01-13 14:05:34 -0800

    Adding migration for company hierarchy

---
+++ database/migrations/2019_01_13_135918_add_node_fields_to_company_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNodeFieldsToCompanyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Companies', function (Blueprint $table) {
+            $table->integer('parent_id')->nullable();
+            $table->integer('lft')->nullable();
+            $table->integer('rgt')->nullable();
+            $table->integer('depth')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Company', function (Blueprint $table) {
+            //
+        });
+    }
+}
