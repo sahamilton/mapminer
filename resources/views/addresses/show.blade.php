57d2026bd6af213b58cf50fde4ba7be79bdd6185
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-02-18 15:29:28 -0800

    Removing reassign button on address

--- resources/views/addresses/show.blade.php
+++ resources/views/addresses/show.blade.php
@@ -23,15 +23,7 @@
   <li><a href="{{route('branches.show',$branch->id)}}">{{$branch->branchname}}</a> - @if(isset($statuses[$branch->pivot->status_id])) 
     {{$statuses[$branch->pivot->status_id]}}
   @endif
-  @if(in_array($branch->id,array_keys($myBranches)))
-  <button type="button" 
-    class="btn btn-warning" 
-
-    data-toggle="modal" 
-    data-target="#reassign">
-      Reassign
-</button>
-  @endif
+  
 </li>
 
   @endforeach
