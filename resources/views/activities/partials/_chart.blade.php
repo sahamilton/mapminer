c4c79d36bd7e656d344c5cced37ae357696c3801
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-03-01 13:05:21 -0800

    Adding consolidated branch dashboard

---
+++ resources/views/activities/partials/_chart.blade.php
@@ -0,0 +1,43 @@
+
+@if(isset($data['summary']) && count($data['summary']) > 0)
+
+<script type="text/javascript" 
+src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+<script>
+
+
+var barChart = new Chart(ctx, 
+{
+
+    type: 'bar',
+
+    resize:true,
+
+    data:{
+      labels: [{!! $data['summary']['chart']['label'] !!}],
+
+      datasets: [
+          
+          {
+              label: "Weekly Activity",
+              backgroundColor: "#ff0000",
+              data:[{!! $data['summary']['chart']['data'] !!}],
+              borderWidth: 1,
+              fill:true,
+          }
+      ]
+    },
+},options = {
+    scales: {
+        xAxes: [{
+            gridLines: {
+                offsetGridLines: true
+            }
+        }]
+    }
+});
+</script>
+
+@endif
+
+</div>
