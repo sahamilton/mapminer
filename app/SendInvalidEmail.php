62eb56ea052eaf0afe204bc7d52fa1a7f1e5684c
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-03-05 07:00:43 -0800

    Adding invalid mail

---
+++ app/SendInvalidEmail.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SendInvalidEmail extends InboundEmail
+{
+        
+    public function getContent(){
+
+    	$content ="<p>We have received an email from  ". $this->inbound->fromEmail."</p>";
+    	$content.= "<p>This email is not registered with any Mapminer user. </p>";
+  
+         $this->content = $content;
+    }
+    
+}
