f11d80dfb9ae9e8108b46c2d58434a49d1059d3a
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-03-06 06:47:18 -0800

    Fixing issue with lead source

--- app/Lead.php
+++ app/Lead.php
@@ -29,7 +29,7 @@ class Lead extends Model  {
             'lng',];
             
 	public $fillable = ['description','address_id'];
- /* public $statuses = [1=>'Offered',2=>'Claimed',3=>'Closed'];
+    public $statuses = [1=>'Offered',2=>'Claimed',3=>'Closed'];
     
     public $getStatusOptions =  [
         1=>'Prospect data is completely inaccurate. No project or project completed.',
@@ -88,7 +88,7 @@ class Lead extends Model  {
 
     }
   
-  */
+
 
   public function createLeadFromGeo($geoCode){
           $coords = $this->getGeoCode($geoCode);
--- resources/views/leadstatus/show.blade.php
+++ resources/views/leadstatus/show.blade.php
@@ -21,7 +21,7 @@
     <td>{{$lead->companyname}}</td>
     <td>{{$lead->city}}</td>
     <td>{{$lead->state}}</td>
-    <td>{{$lead->leadsource->source}}</td>
+    <td>@if($lead->leadsource){{ $lead->leadsource->source}} @endif</td>
     <td>{{$lead->pivot->rating}}</td>
     <td>
         @if($lead->ownedBy->count()>0)
