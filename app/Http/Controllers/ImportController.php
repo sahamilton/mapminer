7721ae0482d99d825a0e8452fc4583d9c75e23b5
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-03-11 18:36:11 -0700

    Fixing export company locations

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -16,7 +16,7 @@ class ImportController extends BaseController
 
     public function index(){
         $imports = ['branches','branch_team','companies','locations','users'];
-        $exports = ['allcompanies','companies','branches','branches_team','companies','person','vertical','nomanager','projects','watch'];
+        $exports = ['allcompanies','branches','branches_team','companies','person','vertical','nomanager','projects','watch'];
         return response()->view('imports.index',compact('imports','exports'));
     }
 
