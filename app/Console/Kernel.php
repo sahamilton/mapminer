faf2dc2527baccae26ab91a47e465a48b8e48934
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-03-15 10:20:38 -0700

    Working on scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->job(new WeeklyActivityReminder())->weekly()->fridays()->at('10:20');
+        $schedule->job(new WeeklyActivityReminder())->weekly()->fridays()->at('10:22');
     }
 
     /**
