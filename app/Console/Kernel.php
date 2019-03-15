82adf31b845b9e1fd9ff1825eff039e434c76c4c
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-03-15 12:40:01 -0700

    Cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->job(new WeeklyActivityReminder())->weekly()->fridays()->at('12:38');
+        $schedule->job(new WeeklyActivityReminder())->weekly()->fridays()->at('12:40');
     }
 
     /**
