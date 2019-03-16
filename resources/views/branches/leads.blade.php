919d75bb601f7a2a8b24ee8f08b9b4be04609e90
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-03-16 08:30:21 -0700

    Working on branch dashboard

---
+++ resources/views/branches/leads.blade.php
@@ -0,0 +1,20 @@
+@extends('site/layouts/default')
+@section('content')
+
+<h2>{{$leads->first()->branch->branchname}} Leads</h2>
+<p><a href="{{route('dashboard.show',$leads->first()->branch->id)}}">Return To Branch Dashboard</a></p>
+<div class="row float-right"><button type="button" 
+    class="btn btn-info float-right" 
+    data-toggle="modal" 
+    data-target="#add_lead">
+      Add Lead
+</button>
+</div>
+@php $route = "branch.leads";
+$branch = $leads->first()->branch;@endphp
+
+@include('branches.partials._selector')
+@include('branches.partials._tableads')
+
+@include('partials._scripts')
+@endsection
