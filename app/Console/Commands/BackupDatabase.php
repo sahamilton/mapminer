22e856a322f9b5d03d76dc9874cfb8432904e725
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-04-03 09:20:57 -0700

    Diagnose backup

--- app/Console/Commands/BackupDatabase.php
+++ app/Console/Commands/BackupDatabase.php
@@ -19,6 +19,10 @@ class BackupDatabase extends Command
     {
         parent::__construct();
         $date = now()->format('Y-m-d hh:mm');
+        dd(config('database.connections.mysql.username'),
+            config('database.connections.mysql.password'),
+            config('database.connections.mysql.database'),
+            storage_path('backups/backup'.$date.'.sql'));
         $this->process = new Process(sprintf(
             'mysqldump -u%s -p%s %s > %s',
             config('database.connections.mysql.username'),
