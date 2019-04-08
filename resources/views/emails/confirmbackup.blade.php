913df770245c5d9421bd843a90f486f7913addb4
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-04-07 19:06:00 -0700

    formatting backup email

--- resources/views/emails/confirmbackup.blade.php
+++ resources/views/emails/confirmbackup.blade.php
@@ -2,7 +2,7 @@
 # Database Backed up
 
 
-Just to confirm that the {{config('app.name')}} database was backed up at {{now()->format('Y-m-d hh:mm')}} to {{$backup}};
+Just to confirm that the {{config('app.name')}} database was backed up at {{now()->format('Y-m-d h:i')}} to {{$backup}};
 
 @component('mail::button', ['url' => ''])
 You can download the db here
