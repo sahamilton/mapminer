ef1256362e60dbec35db4019f6e00a5a6bc60e14
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-04-15 09:12:48 -0700

    Changing schedule of weekly report

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,7 +32,7 @@ class Kernel extends ConsoleKernel
         $schedule->job(new WeeklyActivityReminder())->weekly()->sundays()->at('19:52');
         $schedule->command('db:backup')->dailyAt('23:30');
         $schedule->job(new Top50WeeklyReport())->weekly()->sundays()->at('23:59');
-       $schedule->job(new ActivityOpportunityReport())->weekly()->sundays()->at('23:59');    
+       $schedule->job(new ActivityOpportunityReport())->weekly()->mondays()->at('04:59');    
     }
 
     /**
