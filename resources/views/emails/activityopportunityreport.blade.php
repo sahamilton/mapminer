bf4aa59fea07a480c3a406eaae793a84a46ada96
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-04-14 23:49:53 -0700

    Missing file

--- resources/views/emails/activityopportunityreport.blade.php
+++ resources/views/emails/activityopportunityreport.blade.php
@@ -1,6 +1,7 @@
 @component('mail::message')
 # Sales Appointments vs Opportunitie Won
 
+
 Attached is the weekly report of sales appointments by opportunities won for all branches for the week ended {{$period['to']->format('jS M, Y')}}.
 
 If you no longer need this report please advise Sales Operations.
