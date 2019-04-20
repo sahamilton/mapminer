9bb60e14766ef819e21d6102f7a5963a96f04880
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-04-20 09:09:52 -0700

    Changing url for zip file

--- resources/views/emails/confirmbackup.blade.php
+++ resources/views/emails/confirmbackup.blade.php
@@ -4,7 +4,7 @@
 
 Just to confirm that the {{config('app.name')}} database was backed up at {{now()->format('Y-m-d h:i')}} to {{$backup}}.zip;
 
-@component('mail::button', ['url' => "{!! public_path('storage/backups/'.$backup.'.zip')!!}"])
+@component('mail::button', ['url' => "{{ asset('storage/backups/'.$backup.'.zip') }}"])
 You can download the db here
 @endcomponent
 
