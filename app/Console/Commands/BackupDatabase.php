8b0039197be0bdedee9ac780616f5394b8dbd263
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-04-20 17:58:09 -0700

    Working on zip file

--- app/Console/Commands/BackupDatabase.php
+++ app/Console/Commands/BackupDatabase.php
@@ -44,7 +44,7 @@ class BackupDatabase extends Command
         try { 
             $this->process->mustRun();
             $this->info('The backup has been processed successfully.');
-            ZipBackUp::withChain([new UploadToDropbox($this->filename)
+            ZipBackUp::withChain([UploadToDropbox::dispatch($this->filename)
             ->delay(now()->addMinutes(5)])
             ->dispatch($this->filename)->onQueue('mapminer');
             Mail::queue(new ConfirmBackup($this->filename));
