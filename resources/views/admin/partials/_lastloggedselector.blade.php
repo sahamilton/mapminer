73c10094790343fa2a2183cadad05cb3d1839d80
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-05-03 11:11:13 -0700

    Adding last logged selector

---
+++ resources/views/admin/partials/_lastloggedselector.blade.php
@@ -0,0 +1,20 @@
+<form class="form-inline" action= "{{route('lastlogged')}}" method="post">
+	@csrf
+	<div class="form-group{{ $errors->has('from)') ? ' has-error' : '' }}">
+	    <label 
+		    class="col-md-4 control-label" 
+		    for="lastLogged">Not Logged in Since
+		</label>
+	    <div class="input-group input-group-lg">
+		    <input class="form-control" 
+		        type="text" 
+		        name="fromdatepicker"  
+		        id="fromdatepicker" 
+		        value="{{  old('lastlogged', \Carbon\Carbon::now()->subMonths(1)->format('m/d/Y')) }}"/>
+		    <span class="help-block">
+		        <strong>{{$errors->has('lastlogged') ? $errors->first('lastlogged')  : ''}}</strong>
+		    </span>
+	    </div>
+	    <input type="submit" name="submit" class="btn btn-success" />
+	</div>
+</form>
