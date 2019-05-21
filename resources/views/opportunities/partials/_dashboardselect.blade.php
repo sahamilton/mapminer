91ff22d144cd9a7a9cf1907cefd33c6e7ba3f650
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-05-20 20:27:10 -0700

    Bugsnag issue sales rep / manager dashboard

--- resources/views/opportunities/partials/_dashboardselect.blade.php
+++ resources/views/opportunities/partials/_dashboardselect.blade.php
@@ -1,6 +1,6 @@
 @if($data['team']['me']->user_id != auth()->user()->id)
 
-  @if($data['team']['me']->reports_to != auth()->user()->person->id)
+  @if(isset($data['team']['me']->reports_to) && $data['team']['me']->reports_to != auth()->user()->person->id)
     <p><a href="{{route('manager.dashboard',$data['team']['me']->reports_to)}}">Return to managers dashboard</a></p>
   @endif
   <p><a href="{{route('dashboard.index')}}">Return to my dashboard</a></p>
