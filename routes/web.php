e6c131e339db30d80aedf89cbbd104495fdf0de6
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-05-30 20:59:32 -0700

    Adding activities export

--- routes/web.php
+++ routes/web.php
@@ -45,7 +45,7 @@ Route::group(
            //     Activities/
         Route::get('branch/{branch}/activity/{activitytype?}', ['as'=>'branch.activity', 'uses'=>'ActivityController@getBranchActivtiesByType']);
         Route::get('activity/{activity}/complete', ['as'=>'activity.complete', 'uses'=>'ActivityController@complete']);
-
+        Route::get('activities/export', ['as'=>'activities.export','uses'=>'ActivityContoller@export']);
 
         Route::get('followup', ['as'=>'followup', 'uses'=>'ActivityController@future']);
         Route::resource('activity', 'ActivityController');
