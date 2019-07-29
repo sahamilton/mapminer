742c7b70c9ce99afd37b0e087d55fd603bb92365
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-07-29 14:56:50 -0700

    added agent detect

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,11 +23,17 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $agent = new \Jenssegers\Agent\Agent;
+       
         if (auth()->user()->hasRole(['svp','rvp','evp','market_manager'])) {
             return redirect()->route('dashboard.index');
-        } else if (auth()->user()->hasRole(['branch_manager'])) {
-            return redirect()->route('mobile.index');
-
+        } elseif (auth()->user()->hasRole(['branch_manager'])) {
+            if ($agent->isMobile) {
+                return redirect()->route('mobile.index');  
+            } else {
+                return redirect()->route('dashboard.index');
+            }
+    
         } else {
             return view('welcome');
         }
