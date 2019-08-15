d640ce8b80bda9a8759bdc1a2229c07315c189e8
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-08-14 19:25:57 -0700

    Adding documentation

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -9,14 +9,20 @@ use Maatwebsite\Excel\Concerns\FromView;
 class UsersExport implements FromView
 {
  
-
+    /**
+     * [__construct description]
+     * 
+     * @param [type] $interval [description]
+     */
     public function __construct($interval)
     {
         $this->interval = $interval;
     }
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * [view description]
+     * 
+     * @return [type] [description]
+     */
     public function view(): View
     {
         $users =  User::lastLogin($this->interval)->with('person', 'roles', 'serviceline')->get();
