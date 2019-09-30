66aabf1481c9c706e24ba16a9ede9578470c5bee
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-09-30 12:58:33 -0700

    Setting Opportunity Form Request

--- app/Http/Requests/OpportunityFormRequest.php
+++ app/Http/Requests/OpportunityFormRequest.php
@@ -24,9 +24,34 @@ class OpportunityFormRequest extends FormRequest
     public function rules()
     {
         return [
-            'expected_close'=>'required_if:closed,0|date|nullable|after_or_equal:today',
-            'actual_close'=>'required_if:closed,1,2|date|nullable|before_or_equal:today',
+            'closed'=>'required',
+            'expected_close'=>'required_if:closed,0|date|after_or_equal:today',
+            'actual_close'=>'required_if:closed,1,2|date|before_or_equal:today',
+
            
         ];
+    }
+
+    /**
+     * [messages description]
+     * 
+     * @return [type] [description]
+     */
+    public function messages()
+    {
+        return [
+        'expected_close.required' => 'expected close / actual close is required',
+        
+       
+        ];
+    }
+
+    protected function prepareForValidation()
+    {
+      
+
+        if (! $this->closed == 0) {
+            $this->actual_close = $this->expected_close;
+        }
     }
 }
