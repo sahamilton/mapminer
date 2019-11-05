49254effdc55b2fcdf26b2b5c8d468c343da4c75
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-11-05 09:58:47 -0600

    Refactoring team selector

--- resources/views/campaigns/summary.blade.php
+++ resources/views/campaigns/summary.blade.php
@@ -5,23 +5,10 @@
    <h2>{{$campaign->title}} Summary</h2>
     
         <p><a href="{{route('campaigns.index')}}">Return to all campaigns</a></p>
-        <div class="col-sm-6">
-            <form
-                name="teamselector"
-                id="teamselector"
-                method="post"
-                action="{{route('campaigns.report', $campaign->id)}}"
-                >
-            @csrf
+        
             @include('campaigns.partials._teamselector')
-        <div class="form-group">
-            <input type="submit" 
-                class="btn btn-info"
-                value="Select"
-                />
-            </div>
-    </form>
-</div>
+        
+
     <table id="sorttable"
         name="branchsummary"
         class="table table-striped"
