bdc63b886c7c4e3b9094fa81d05c7a2e5815cadc
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-11-10 17:03:48 -0600

    Adding create / edit to salesnotes

--- resources/views/salesnotes/shownote.blade.php
+++ resources/views/salesnotes/shownote.blade.php
@@ -4,6 +4,12 @@
 {{-- Page content --}}
 @section('content')
 <div class="container">
+@if(auth()->user()->hasRole('admin'))
+<div class="float-right">
+    <a href="{{route('salesnotes.edit', $company->id)}}" class="btn btn-info" value="Create / Edit" >Create / Edit </a>
+</div>
+
+@endif
 @include('salesnotes.partials._shownote')
 </div>
 @include('partials._scripts')
