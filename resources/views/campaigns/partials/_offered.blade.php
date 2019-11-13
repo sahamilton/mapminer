80b56801688fe69c0a386b65b099d1e37a0e6d76
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-11-13 17:10:36 -0600

    Adding limit to offered Leads

--- resources/views/campaigns/partials/_offered.blade.php
+++ resources/views/campaigns/partials/_offered.blade.php
@@ -9,19 +9,20 @@
         <th>ZIP</th>
     </thead>
     <tbody>
-       
         @foreach ($branch->offeredLeads as $lead)
-        <tr>
-            <td>
-                <a href="{{route('address.show', $lead->id)}}">
-                    {{$lead->businessname}}
-                </a>
-            </td>
-            <td>{{$lead->street}}</td>
-            <td>{{$lead->city}}</td>
-            <td>{{$lead->state}}</td>
-            <td>{{$lead->zip}}</td>
-        </tr>
+            @if($loop->iteration > 6)
+                <tr>
+                    <td>
+                        <a href="{{route('address.show', $lead->id)}}">
+                            {{$lead->businessname}}
+                        </a>
+                    </td>
+                    <td>{{$lead->street}}</td>
+                    <td>{{$lead->city}}</td>
+                    <td>{{$lead->state}}</td>
+                    <td>{{$lead->zip}}</td>
+                </tr>
+            @endif
         @endforeach
     </tbody>
 </table>
