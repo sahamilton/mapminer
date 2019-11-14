86846df48e0225c6fc5023bf83f7cec295d4b217
Stephen Hamilton <Stephen@CrescentCreative.com>
2019-11-14 14:39:27 -0600

    Fixing activities show

---
+++ resources/views/activities/show.blade.php
@@ -0,0 +1,29 @@
+@extends('site.layouts.default')
+@section('content')
+<div class="container">
+<h4>Activity at {{$activity->relatesToAddress->businessname}}</h4>
+<p><a href="{{route('address.show', $activity->relatesToAddress->id)}}">Return to address</a></p>
+<p><strong>Recorded by:</strong>
+    @if($activity->user)
+    {{$activity->user->person->fullName()}}
+    @else
+    No longer a Mapminer User
+    @endif
+</p>
+<p><strong>Branch:</strong>
+    @if($activity->branch)
+    {{$activity->branch->branchname}}
+    @else
+    No branch recorded
+    @endif
+</p>
+<p><strong>Date:</strong>
+    {{$activity->activity_date->format('Y-m-d')}}</p>
+<p><strong>Activity:</strong>
+    {{$activity->type->activity}}</p>
+<p><strong>Details:</strong>
+    {{$activity->note}}</p>
+
+</div>
+
+@endsection
