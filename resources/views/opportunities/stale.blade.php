d33df06a281a0d94cc1606795ff10d1620d29a05
Stephen Hamilton <Stephen@CrescentCreative.com>
2020-03-10 19:30:28 -0600

    Reformatting stale opportunities report

--- resources/views/opportunities/stale.blade.php
+++ resources/views/opportunities/stale.blade.php
@@ -1,6 +1,14 @@
-@foreach ($branches as $branch)
-<tr>
-    <td>{{$branch->branchname}}</td>
-    <td>{{$branch->stale_opportunities_count}}</td>
-</tr>
-@endforeach
+<table>
+    <thead>
+        <th>Branch</th>
+        <th>Stale Opportunity Count</th>
+    </thead>
+    <tbody>
+        @foreach ($branches as $branch)
+            <tr>
+                <td>{{$branch->branchname}}</td>
+                <td>{{$branch->stale_opportunities_count}}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
