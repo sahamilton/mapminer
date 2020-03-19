4d177ba2bdab4435ba5816210f165d5e6a68ae4a
Stephen Hamilton <Stephen@CrescentCreative.com>
2020-03-19 09:31:26 -0700

    Removing debug

--- resources/views/myleads/branches.blade.php
+++ resources/views/myleads/branches.blade.php
@@ -38,22 +38,18 @@
 
  <table id ='sorttable' class='table table-striped table-bordered table-condensed table-hover'>
     <thead>
-
-    <th>Company</th>
-    <th>Date Added</th>
-    <th>Address</th>
-    <th>Lead Source</th>
-    <th>Last Campaign</th>
-    <th>Last Activity</th>
-    <th>Remove</th>
-
+        <th>Company</th>
+        <th>Date Added</th>
+        <th>Address</th>
+        <th>Lead Source</th>
+        <th>Last Campaign</th>
+        <th>Last Activity</th>
+        <th>Remove</th>
     </thead>
     <tbody>
 
     @foreach($branch->leads as $lead)
-   
     <tr>
-        
         <td>
             <a href="{{route('address.show',$lead->id)}}">
                 {{$lead->businessname}}
