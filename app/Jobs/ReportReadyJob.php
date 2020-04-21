3b650bddda0193a9f64f700cb6d90a313e306981
Stephen Hamilton <Stephen@CrescentCreative.com>
2020-04-21 07:22:26 -0700

    Removing duplicate send report

--- app/Jobs/ReportReadyJob.php
+++ app/Jobs/ReportReadyJob.php
@@ -11,7 +11,6 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use App\Mail\SendReport;
 
 class ReportReadyJob implements ShouldQueue
 {
