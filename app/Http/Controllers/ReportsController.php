dd81501c7201319623b635f83448ad10ffefac72
Stephen Hamilton <Stephen@CrescentCreative.com>
2020-04-21 20:01:33 -0700

    Debugging reports

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -245,7 +245,7 @@ class ReportsController extends Controller {
                 //dd($export, $report->job, $report);
                 //return (new InvoicesExport)->download('invoices.xlsx');
                 //
-                return (new \App\Exports\DailyBranchExport($period, $myBranches))->download($report->job . '.xlsx');
+                return (new \App\Exports\DailyBranchExport($period, $myBranches));
 
 
             
