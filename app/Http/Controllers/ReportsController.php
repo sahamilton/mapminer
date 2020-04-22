07b9dcb1ef6dba8bd959fa71297117b8cd9a0f88
Stephen Hamilton <Stephen@CrescentCreative.com>
2020-04-21 19:12:04 -0700

    debugging reports

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -241,34 +241,13 @@ class ReportsController extends Controller {
             
             $export = "\App\Exports\\". $report->export;
             
-            switch ($report->object) {
-            case 'Company':
 
-                $company = $this->company->findOrFail(request('company'));
-                return Excel::download(new $export($company, $period, $myBranches), $company->companyname . " " . $report->job . 'Activities.csv');
-                break;
-
-            case 'Role':
-               
-                return Excel::download(new $export(request('role'), $team), $report->job . '.csv');
-                break;
-
-            case 'User':
-                
-                return Excel::download(new $export($period, [$manager->id]), $report->job . '.csv');
-                break;
-
-            case 'Campaign':
-
-                return Excel::download(new $export([$manager->id], $campaign), $report->job . '.csv');
-                break;
-
-            default:
                 //dd($export, $report->job, $report);
-                return Excel::download(new \App\Exports\DailyBranchExport($period, $myBranches), $report->job . '.csv');
-                break;
+                //return (new InvoicesExport)->download('invoices.xlsx');
+                return (new \App\Exports\DailyBranchExport($period, $myBranches))->download($report->job . '.csv');
 
-            } 
+
+            
             
         } else {
             return redirect()->route('welcome');
