d82fdf72d5d38119b281cf767132bc87b702972e
Stephen Hamilton <Stephen@CrescentCreative.com>
2020-04-28 08:19:32 -0700

    Working on report distribution

--- resources/views/reports/partials/_index.blade.php
+++ resources/views/reports/partials/_index.blade.php
@@ -9,6 +9,7 @@
 <table id ='responsive8' class='table table-striped table-bordered table-condensed table-hover'>
     <thead>
         <th>Report</th>
+        <th>Job</th>
         <th>Description</th>
         @if(auth()->user()->hasRole('admin'))
         <th>Public</th>
@@ -27,7 +28,7 @@
                     {{$report->report}}
                 @endif
             </td>
-           
+           <td>{{$report->job}}</td>
             <td>{{ucwords($report->description)}}</td>            
              @if(auth()->user()->hasRole('admin'))
                 <td>
