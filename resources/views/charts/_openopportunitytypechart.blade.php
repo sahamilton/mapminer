bf68fcedb801a8d5007656a58029c3184d91119b
Stephen Hamilton <Stephen@CrescentCreative.com>
2020-05-20 07:23:31 -0700

    NAM Dashboards

---
+++ resources/views/charts/_openopportunitytypechart.blade.php
@@ -0,0 +1,66 @@
+<script>
+
+
+var numberWithCommas = function(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+var leadsTypeChart = new Chart(ctopportunities, 
+{
+    type: 'bar',
+
+    resize:true,
+
+    data:{
+      labels: ['{!!  $data['team']['activitytypechart']['labels'] !!}'],
+      datasets: [
+       @foreach ($data['team']['opportunitytypechart']['data'] as $key=>$value) 
+       
+        {
+          
+          label: '{!! $key !!}',
+          data: [{{$value['data']}}],
+          backgroundColor:'{{$value['color']}}',
+        },
+      @endforeach      
+     ]
+    },
+
+    options: {
+        
+        animation: {
+          duration: 5,
+        },
+        tooltips: {
+          mode: 'label',
+          callbacks: {
+          label: function(tooltipItem, data) { 
+            return data.datasets[tooltipItem.datasetIndex].label + ": " + numberWithCommas(tooltipItem.yLabel);
+          }
+          }
+         },
+        scales: {
+          xAxes: [{ 
+            stacked: true, 
+            gridLines: { display: false },
+             ticks: {
+                  autoSkip:false,
+                  beginAtZero: true   // minimum value will be 0.
+              },
+            }],
+          yAxes: [{ 
+            stacked: true, 
+             ticks: {
+                  autoSkip:false,
+                  beginAtZero: true ,  // minimum value will be 0.
+            
+              callback: function(value) { return numberWithCommas(value); },
+            }, 
+            }],
+        }, // scales
+        legend: {display: true}
+    } // options
+  }
+);
+
+
+</script>
