2112d7c61dd1d5a2d48649d575b482672877b303
Stephen Hamilton <Stephen@CrescentCreative.com>
2020-07-15 08:58:42 -0700

    Changing perPage to include 50 & 100

--- resources/views/livewire/lead-table.blade.php
+++ resources/views/livewire/lead-table.blade.php
@@ -4,8 +4,9 @@
             Per Page: &nbsp;
             <select wire:model="perPage" class="form-control">
                 <option>10</option>
-                <option>15</option>
                 <option>25</option>
+                <option>50</option>
+                <option>100</option>
             </select>
         </div>
 
