f23af071bcf74be1a1ddc737c9f051b1c6c61aa5
Stephen Hamilton <Stephen@CrescentCreative.com>
2020-07-19 12:40:08 -0700

    Adding check on session manager vs my team

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -41,8 +41,9 @@ class DashboardController extends Controller
      */
     public function index()
     {
+        $myTeam = auth()->user()->person->myTeam()->get()->pluck('user_id')->toArray();
         
-        if(! session('manager')) {
+        if(! session('manager') or ! in_array(session('manager'), $myTeam)) {
             session(['manager'=>auth()->user()->id]);
         }
         
@@ -172,7 +173,7 @@ class DashboardController extends Controller
         $manager = $this->person->findOrFail(request('manager'));
 
         session(['manager'=>$manager->user_id]);
-   
+       
         return redirect()->route('dashboard.index');
     }
 
