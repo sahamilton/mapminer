92d1b6336a939f7a6d04a1bc0226b1772808a25c
Stephen Hamilton <Stephen@CrescentCreative.com>
2020-08-13 11:31:18 -0700

    Adding company id to import

--- app/Http/Controllers/LocationsImportController.php
+++ app/Http/Controllers/LocationsImportController.php
@@ -42,7 +42,7 @@ class LocationsImportController extends ImportController
     public function import(Request $request) 
     {
         $data = request()->except('_token');
-        
+
         $title="Map the locations import file fields";
         $data = array_merge($data, $this->uploadfile(request()->file('upload')));
         $data['additionaldata'] = null;
@@ -55,7 +55,7 @@ class LocationsImportController extends ImportController
         if (request()->filled('company')) {
                 $data['additionaldata']['company_id'] = request('company');
                 $this->import->setDontCreateTemp(true); 
-
+                $company_id = request('company');
         } else {
                 $this->import->setDontCreateTemp(false);
         }
