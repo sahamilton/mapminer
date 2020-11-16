ea35f376d3610f83d06b70726a20be8dcf5f1fc3
James Roe <58564719+james-webdev@users.noreply.github.com>
2020-11-16 12:08:36 +0000

    testing git on dev branch

--- resources/views/site/layouts/_navbar.blade.php
+++ resources/views/site/layouts/_navbar.blade.php
@@ -31,7 +31,7 @@
                 data-toggle="dropdown"
                 aria-haspopup="true"
                 aria-expanded="false">
-                  <span><i class="fas fa-search"></i> Search</span>
+                  <span><i class="fas fa-search"></i> Searchi</span>
                   <svg class="text-gray-400 h-5 w-5 group-hover:text-gray-500 group-focus:text-gray-500 transition        ease-in-out duration-150" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                   </svg>
