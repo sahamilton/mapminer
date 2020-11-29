476972e1ccff7777b4da35eabd228cefa61daf5a
Stephen Hamilton <Stephen@CrescentCreative.com>
2020-11-29 08:18:21 -0800

    Reformatting user tracking

--- resources/views/usertracking/detail.blade.php
+++ resources/views/usertracking/detail.blade.php
@@ -1,28 +1,31 @@
 @extends('site.layouts.default')
 @section('content')
 
-<div class="container">
-    <h2>{{$model}} created by {{$user->person->fullName()}}</h2>
-    
-    <p><a href="{{route('usertracking.index')}}">Return to all user tracking</a></p>
-   @switch ($model) 
-    @case ('Opportunity')
+    <div class="container">
+        <h2>{{$model}} created by {{$user->person->fullName()}}</h2>
+        
+        <p>
+            <a href="{{route('usertracking.index')}}">Return to all user tracking</a>
+        </p>
+        @switch ($model) 
+            
+            @case ('Opportunity')
+                @livewire('usertrack-opportunities', ['period'=>$period, 'user'=>$user])
+                @break
+            
+            @case('Activity')
+                @livewire('usertrack-activities', ['period'=>$period, 'user'=>$user]) 
+                @break
 
-        @livewire('usertrack-opportunities', ['period'=>$period, 'user'=>$user])
-       @break
-    @case('Activity')
-        @livewire('usertrack-activities', ['period'=>$period, 'user'=>$user]) 
-    @break
+            @case ('Address') 
+                @livewire('usertrack-activities', ['period'=>$period, 'user'=>$user]) 
+                @break
 
-    @case ('Address') 
-        @livewire('usertrack-activities', ['period'=>$period, 'user'=>$user]) 
-    @break
+            @case ('Track')
+                @livewire('usertrack-logins', ['period'=>$period, 'user'=>$user])
+                @break
 
-    @case ('Track')
-        {{dd("Sorry I havent written that yet');")}}
-    @break
+        @endswitch
 
-    @endswitch
-
-</div>
+    </div>
 @endsection
