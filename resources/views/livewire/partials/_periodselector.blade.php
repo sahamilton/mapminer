7730ef89375e4a0ab085d66fd4e4061ee80efa50
Stephen Hamilton <Stephen@CrescentCreative.com>
2020-12-02 10:52:40 -0800

    Removing all from livewire period selector

--- resources/views/livewire/partials/_periodselector.blade.php
+++ resources/views/livewire/partials/_periodselector.blade.php
@@ -4,7 +4,7 @@
             <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
         </div>
         <select wire:model="setPeriod" class="form-control">
-           <option value="All">All</option>
+           
             @foreach (config('mapminer.timeframes') as $key=>$per)
                 <option
                 value="{{$key}}">{{$per}}</option>
