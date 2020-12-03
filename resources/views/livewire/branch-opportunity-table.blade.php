b3f1a066a69858b1797053be777e14193122e488
Stephen Hamilton <Stephen@CrescentCreative.com>
2020-12-02 17:29:13 -0800

    Adding totals to opportunities summary table

--- resources/views/livewire/branch-opportunity-table.blade.php
+++ resources/views/livewire/branch-opportunity-table.blade.php
@@ -28,18 +28,24 @@
                     </td>
                     <td align="center">
                         {{$branch->open_opportunities}}
-                        
-                        
+
                     </td>
                     <td align="center">{{$branch->new_opportunities}}</td>
                     <td align="right">{{$branch->open_value ? "$" . number_format($branch->open_value,0) : '0'}}</td>
                     <td align="center">{{$branch->won_opportunities}}</td>
-                   <td align="right">{{$branch->won_value ? "$" . number_format($branch->won_value,0) : '0'}}</td>
+                    <td align="right">{{$branch->won_value ? "$" . number_format($branch->won_value,0) : '0'}}</td>
                    
                 </tr>
                 @endforeach
-            </tbody>
-            
+            </tbody><td>Totals</td>
+            <td align="center">{{$branches->sum('open_opportunities')}}</td>
+            <td align="center">{{$branches->sum('new_opportunities')}}</td>
+            <td align="right">${{number_format($branches->sum('open_value'),0)}}</td>
+            <td align="center">{{$branches->sum('won_opportunities')}}</td>
+            <td align="right">${{number_format($branches->sum('won_value'),0)}}</td>
+            <tfoot>
+
+            </tfoot>
         </table>
     </div>
         <div class="row">
