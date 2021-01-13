62eee56926896bf4351840bc905b1f12ff933ffc
Stephen Hamilton <Stephen@CrescentCreative.com>
2021-01-13 12:22:42 -0800

    REmoving sort by company name in activities table livewire

--- resources/views/livewire/activities-table.blade.php
+++ resources/views/livewire/activities-table.blade.php
@@ -64,10 +64,9 @@
     <table  class='table table-striped table-bordered table-condensed table-hover'>
         <thead>
             <th>
-                <a wire:click.prevent="sortBy('businessname')" role="button" href="#">
+                
                     Company
-                    @include('includes._sort-icon', ['field' => 'activity_date'])
-                </a>
+                   
             </th>
             <th>
                 <a wire:click.prevent="sortBy('activity_date')" role="button" href="#">
