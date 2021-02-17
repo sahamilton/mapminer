4f649695ed93940adea838281d7b6a3c80b79edd
Stephen Hamilton <Stephen@CrescentCreative.com>
2021-02-17 07:35:13 -0800

    Removing Morris Laptop

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -154,6 +154,13 @@
       0 => 'McCool\\LaravelAutoPresenter\\AutoPresenterServiceProvider',
     ),
   ),
+  'morrislaptop/laravel-queue-clear' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Morrislaptop\\LaravelQueueClear\\LaravelQueueClearServiceProvider',
+    ),
+  ),
   'nesbot/carbon' => 
   array (
     'providers' => 
