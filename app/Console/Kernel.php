39f9ad2ec245653a23dc849d812ee76dd29d0fbd
Stephen Hamilton <Stephen@CrescentCreative.com>
2021-04-07 14:49:27 -0700

    Updating job schedule (kernel)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Jobs\WeeklyActivityReminder;
+use App\Jobs\WeeklySummary;
 use App\Jobs\Top25WeeklyReport;
 use App\Jobs\ActivityOpportunity;
 use App\Jobs\AccountActivities;
