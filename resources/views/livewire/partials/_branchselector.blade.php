55a7f49e5509e810e10c81a14ec80a8184ebc95e
Stephen Hamilton <Stephen@CrescentCreative.com>
2021-04-09 11:06:02 -0700

    Reformatting branch selector

--- resources/views/livewire/partials/_branchselector.blade.php
+++ resources/views/livewire/partials/_branchselector.blade.php
@@ -1,17 +1,16 @@
 @php $myBranches = auth()->user()->person->myBranches(); @endphp
 @if(count($myBranches)>1)
-    <div class="col-sm-4">
-        <div class="input-group-prepend">
-            <span class="input-group-text"><i class="fab fa-pagelines"></i></span>
-        </div>
-        <select wire:model="branch_id" 
+<div class="col form-inline"> 
+    <div class="input-group-prepend">
+        <span class="input-group-text"><i class="fab fa-pagelines"></i></span>
+    </div>
+    <select wire:model="branch_id" 
         class="form-control input-sm" 
         id="branchselect" 
         name="branch">
-            
-              @foreach ($myBranches as $key=>$branchname)
-                    <option value="{{$key}}">{{$branchname}}</option>
-              @endforeach 
-        </select>
-    </div>
+        @foreach ($myBranches as $key=>$branchname)
+            <option value="{{$key}}">{{$branchname}}</option>
+        @endforeach 
+    </select>
+</div>
 @endif  
