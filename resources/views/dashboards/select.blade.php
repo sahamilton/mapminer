4366efa290e5e069b2677452659e0fe07873223c
Stephen Hamilton <Stephen@CrescentCreative.com>
2021-06-15 09:16:05 -0700

    Debugging dashboard selector

--- resources/views/dashboards/select.blade.php
+++ resources/views/dashboards/select.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.default')
 @section('content')
 <div class="container" name="selectManagerDashboard" >
-    <h2>Select Managers Dashboard</h2>
+    
     @livewire('dashboard-table')
    
 </div>
