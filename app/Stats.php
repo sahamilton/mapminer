2f20d7b3b8b7876f1d0fd97cbcc4be35259f7155
Stephen Hamilton <Stephen@CrescentCreative.com>
2021-07-05 11:22:42 -0700

    Refactoring mapminer stats; adding new branch metric

--- app/Stats.php
+++ app/Stats.php
@@ -27,187 +27,19 @@ class Stats extends Model
 
     }
 
-    
-    public function getUsageStats()
+    /**
+     * [getUsageStats description]
+     * 
+     * @return [type] [description]
+     */
+    public function getUsageStats() : array
     {
-        
-       
+               
         return [
-            'current'=>
-            [
-                'logins' => Track::whereBetween('lastactivity', [$this->period['from'], $this->period['to']])
-                    ->when(
-                        $this->team, function ($q) {
-                            $q->whereIn('user_id', array_keys($this->team));
-                        }
-                    )
-                    ->count(),
-                'registered_users'=>User::where('created_at', '<=', $this->period['to'])
-                    ->when(
-                        $this->team, function ($q) {
-                            $q->whereIn('id', array_keys($this->team));
-                        }
-                    )->count(),
-                'active_users' => Track::whereBetween('lastactivity', [$this->period['from'], $this->period['to']])
-                    ->when(
-                        $this->team, function ($q) {
-                            $q->whereIn('user_id', array_keys($this->team));
-                        }
-                    )
-                    ->distinct('user_id')->count(),
-                'new_users' => User::withTrashed()->whereBetween('created_at', [$this->period['from'], $this->period['to']])
-                    ->when(
-                        $this->team, function ($q) {
-                            $q->whereIn('id', array_keys($this->team));
-                        }
-                    )->count(),
-                'deleted_users' => User::withTrashed()->whereBetween('deleted_at', [$this->period['from'], $this->period['to']])
-                    ->when(
-                        $this->team, function ($q) {
-                            $q->whereIn('id', array_keys($this->team));
-                        }
-                    )
-                    ->count(),
-                'active_branches' => Branch::wherehas(
-                    'activities', function ($q) {
-                        $q->whereBetween('activity_date',  [$this->period['from'], $this->period['to']]);
-                    }
-                )
-                ->count(),
-                'inactive_branches' => Branch::whereDoesntHave(
-                    'activities', function ($q) {
-                        $q->whereBetween('activity_date',  [$this->period['from'], $this->period['to']]);
-                    }
-                )
-                ->count(),
-                'activities' => Activity::completed()->whereBetween('activity_date', [$this->period['from'], $this->period['to']])
-                    ->when(
-                        $this->branches, function ($q) {
-                            $q->whereIn('branch_id', $this->branches);
-                        }
-                    )->count(),
-                'new_leads' => Address::whereBetween('created_at', [$this->period['from'], $this->period['to']])
-                    ->when(
-                        $this->team, function ($q) {
-                            $q->whereIn('user_id', array_keys($this->team));
-                        }
-                    )->count(),
-                
-                'new_opportunities' => Opportunity::whereBetween('created_at', [$this->period['from'], $this->period['to']])
-                    ->when(
-                        $this->branches, function ($q) {
-                            $q->whereIn('branch_id', $this->branches);
-                        }
-                    )->count(),
-                
-                'new_opportunities_value' => Opportunity::whereBetween('created_at', [$this->period['from'], $this->period['to']])
-                    ->when(
-                        $this->branches, function ($q) {
-                            $q->whereIn('branch_id', $this->branches);
-                        }
-                    )->sum('value'),
-                
-                'won_opportunities' => Opportunity::whereBetween('actual_close', [$this->period['from'], $this->period['to']])
-                    ->when(
-                        $this->branches, function ($q) {
-                            $q->whereIn('branch_id', $this->branches);
-                        }
-                    )
-                    ->where('closed', 1)
-                    ->count(),
-                
-                'won_value' => Opportunity::whereBetween('actual_close', [$this->period['from'], $this->period['to']])
-                    ->where('closed', 1)
-                    ->when(
-                        $this->branches, function ($q) {
-                            $q->whereIn('branch_id', $this->branches);
-                        }
-                    )
-                    ->sum('value'),
-            ],
-            'prior'=>
-            [
-
-                'logins' => Track::whereBetween('lastactivity', [$this->priorPeriod['from'], $this->priorPeriod['to']])
-                    ->when(
-                        $this->team, function ($q) {
-                            $q->whereIn('user_id', array_keys($this->team));
-                        }
-                    )
-                    ->count(),
-                'registered_users'=>User::where('created_at', '<=', $this->priorPeriod['to'])
-                    ->when(
-                        $this->team, function ($q) {
-                            $q->whereIn('id', array_keys($this->team));
-                        }
-                    )->count(),
-                'active_users' => Track::whereBetween('lastactivity', [$this->priorPeriod['from'], $this->priorPeriod['to']])
-                    ->when(
-                        $this->team, function ($q) {
-                            $q->whereIn('user_id', array_keys($this->team));
-                        }
-                    )
-                    ->distinct('user_id')->count(),
-                'new_users' => User::withTrashed()->whereBetween('created_at', [$this->priorPeriod['from'], $this->priorPeriod['to']])
-                    ->when(
-                        $this->team, function ($q) {
-                            $q->whereIn('id', array_keys($this->team));
-                        }
-                    )->count(),
-                'deleted_users' => User::withTrashed()->whereBetween('deleted_at', [$this->priorPeriod['from'], $this->priorPeriod['to']])
-                    ->when(
-                        $this->team, function ($q) {
-                            $q->whereIn('id', array_keys($this->team));
-                        }
-                    )
-                    ->count(),
-                'activities' => Activity::completed()->whereBetween('activity_date', [$this->priorPeriod['from'], $this->priorPeriod['to']])
-                    ->when(
-                        $this->branches, function ($q) {
-                            $q->whereIn('branch_id', $this->branches);
-                        }
-                    )->count(),
-                'new_leads' => Address::whereBetween('created_at', [$this->priorPeriod['from'], $this->priorPeriod['to']])
-                    ->when(
-                        $this->team, function ($q) {
-                            $q->whereIn('user_id', array_keys($this->team));
-                        }
-                    )->count(),
-                
-                'new_opportunities' => Opportunity::whereBetween('created_at', [$this->priorPeriod['from'], $this->priorPeriod['to']])
-                    ->when(
-                        $this->branches, function ($q) {
-                            $q->whereIn('branch_id', $this->branches);
-                        }
-                    )->count(),
-                
-                'new_opportunities_value' => Opportunity::whereBetween('created_at', [$this->priorPeriod['from'], $this->priorPeriod['to']])
-                    ->when(
-                        $this->branches, function ($q) {
-                            $q->whereIn('branch_id', $this->branches);
-                        }
-                    )->sum('value'),
-                
-                'won_opportunities' => Opportunity::whereBetween('actual_close', [$this->priorPeriod['from'], $this->priorPeriod['to']])
-                    ->when(
-                        $this->branches, function ($q) {
-                            $q->whereIn('branch_id', $this->branches);
-                        }
-                    )
-                    ->where('closed', 1)
-                    ->count(),
-                
-                'won_value' => Opportunity::whereBetween('actual_close', [$this->priorPeriod['from'], $this->priorPeriod['to']])
-                    ->where('closed', 1)
-                    ->when(
-                        $this->branches, function ($q) {
-                            $q->whereIn('branch_id', $this->branches);
-                        }
-                    )
-                    ->sum('value'),
-
-
-            ],
+            'current'=> $this->metrics($this->period),
+            
+            'prior'=> $this->metrics($this->priorPeriod),
+            
             'period'=>[
                 'current'=>$this->period,
                 'prior'=>$this->priorPeriod,
@@ -216,6 +48,11 @@ class Stats extends Model
 
         
     }
+    /**
+     * _getPriorPeriod  Calculate appropriate prior perdiod
+     * 
+     * @return [type] [description]
+     */
     private function _getPriorPeriod()
     {
 
@@ -244,5 +81,100 @@ class Stats extends Model
         return $this->manager->getMyBranches();
     }
 
+
+    private function metrics(array $period) : array
+    {
+        return [
+                'logins' => Track::whereBetween('lastactivity', [$period['from'], $period['to']])
+                    ->when(
+                        $this->team, function ($q) {
+                            $q->whereIn('user_id', array_keys($this->team));
+                        }
+                    )
+                    ->count(),
+                'registered_users'=>User::where('created_at', '<=', $period['to'])
+                    ->when(
+                        $this->team, function ($q) {
+                            $q->whereIn('id', array_keys($this->team));
+                        }
+                    )->count(),
+                'active_users' => Track::whereBetween('lastactivity', [$period['from'], $period['to']])
+                    ->when(
+                        $this->team, function ($q) {
+                            $q->whereIn('user_id', array_keys($this->team));
+                        }
+                    )
+                    ->distinct('user_id')->count(),
+                'new_users' => User::withTrashed()->whereBetween('created_at', [$period['from'], $period['to']])
+                    ->when(
+                        $this->team, function ($q) {
+                            $q->whereIn('id', array_keys($this->team));
+                        }
+                    )->count(),
+                'deleted_users' => User::withTrashed()->whereBetween('deleted_at', [$period['from'], $period['to']])
+                    ->when(
+                        $this->team, function ($q) {
+                            $q->whereIn('id', array_keys($this->team));
+                        }
+                    )
+                    ->count(),
+                'active_branches' => Branch::wherehas(
+                    'activities', function ($q) use ($period) {
+                        $q->whereBetween('activity_date',  [$period['from'], $period['to']]);
+                    }
+                )
+                ->count(),
+                'inactive_branches' => Branch::whereDoesntHave(
+                    'activities', function ($q) use ($period) {
+                        $q->whereBetween('activity_date',  [$period['from'], $period['to']]);
+                    }
+                )
+                ->count(),
+                'activities' => Activity::completed()->whereBetween('activity_date', [$period['from'], $period['to']])
+                    ->when(
+                        $this->branches, function ($q) {
+                            $q->whereIn('branch_id', $this->branches);
+                        }
+                    )->count(),
+                'new_leads' => Address::whereBetween('created_at', [$period['from'], $period['to']])
+                    ->when(
+                        $this->team, function ($q) {
+                            $q->whereIn('user_id', array_keys($this->team));
+                        }
+                    )->count(),
+                
+                'new_opportunities' => Opportunity::whereBetween('created_at', [$period['from'], $period['to']])
+                    ->when(
+                        $this->branches, function ($q) {
+                            $q->whereIn('branch_id', $this->branches);
+                        }
+                    )->count(),
+                
+                'new_opportunities_value' => Opportunity::whereBetween('created_at', [$period['from'], $period['to']])
+                    ->when(
+                        $this->branches, function ($q) {
+                            $q->whereIn('branch_id', $this->branches);
+                        }
+                    )->sum('value'),
+                
+                'won_opportunities' => Opportunity::whereBetween('actual_close', [$period['from'], $period['to']])
+                    ->when(
+                        $this->branches, function ($q) {
+                            $q->whereIn('branch_id', $this->branches);
+                        }
+                    )
+                    ->where('closed', 1)
+                    ->count(),
+                
+                'won_value' => Opportunity::whereBetween('actual_close', [$period['from'], $period['to']])
+                    ->where('closed', 1)
+                    ->when(
+                        $this->branches, function ($q) {
+                            $q->whereIn('branch_id', $this->branches);
+                        }
+                    )
+                ->sum('value'),
+            ];
+    }
 
 }
