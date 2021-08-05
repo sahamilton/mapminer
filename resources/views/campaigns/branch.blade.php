780192baca66e9bebc3095075793f4ef970a57c1
Stephen Hamilton <Stephen@CrescentCreative.com>
2021-08-05 09:49:38 -0700

    Working on multi branch views

--- resources/views/campaigns/branch.blade.php
+++ resources/views/campaigns/branch.blade.php
@@ -2,10 +2,10 @@
 @section('content')
 <div class="container">
   
-    @if(count(auth()->user()->person->getMyBranches()) > 1)
+    @if(count(auth()->user()->person->getMyBranches()) > 1 && ! isset($branch))
         @livewire('campaign-tracking')
     @else
-        @livewire('branch-campaign')
+        @livewire('branch-campaign', ['branch_id'=>$branch->id])
     @endif
 </div>
 
