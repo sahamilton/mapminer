3151142d58c272df998b4b4c14c42da5d20057d5
Stephen Hamilton <Stephen@CrescentCreative.com>
2021-08-05 17:59:36 -0700

    Changing queue job for assign leads

---
+++ app/Jobs/InsertLeadsToBranchJob.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+
+use App\AddressBranch;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class InsertLeadsToBranchJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    public $leads;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(array $leads)
+    {
+        $this->leads = $leads;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        AddressBranch::insert($this->leads);
+    }
+}
