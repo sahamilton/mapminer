6fe75301eb8bb735de13660d768234e567cfe134
Stephen Hamilton <Stephen@CrescentCreative.com>
2021-08-15 13:17:40 -0700

    Restoring all branches

--- app/Jobs/BranchCampaign.php
+++ app/Jobs/BranchCampaign.php
@@ -44,7 +44,7 @@ class BranchCampaign implements ShouldQueue
         return Branch::has('manager')
             ->active()
             
-            ->whereIn('id', [1265])
+            ->whereIn('id', $this->campaign->branches->pluck('id')->toArray())
             ->get();
 
     }
