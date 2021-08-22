355a91b6304f90b6a4c771e731fee0ef167cd6f8
Stephen Hamilton <Stephen@CrescentCreative.com>
2021-08-22 07:33:20 -0700

    refacotring branch dashboard with branch id

--- app/Http/Controllers/BranchDashboardController.php
+++ app/Http/Controllers/BranchDashboardController.php
@@ -154,7 +154,7 @@ class BranchDashboardController extends DashboardController
     public function show(Branch $branch)
     {
         $myBranches = $this->person->myBranches();
-        
+
          
         if (! array_key_exists($branch->id, $myBranches)) {
              return redirect()->back()
@@ -188,9 +188,9 @@ class BranchDashboardController extends DashboardController
                 );
         }
         $campaigns = Campaign::currentOpen([$branch->id])->get();;
-        $this->myBranches = array_keys($myBranches);
+        $this->myBranches = [$branch->id];
         $data = $this->_getDashBoardData();
-        
+
         $data['mybranches'] = Branch::whereIn('id', array_keys($myBranches))->pluck('branchname', 'id');
      
         return response()->view('branches.dashboard', compact('data', 'branch', 'campaigns', 'myBranches'));
@@ -220,6 +220,7 @@ class BranchDashboardController extends DashboardController
 
         }
         $data['charts'] = $this->_getCharts($data);
+       
         return $data;
     }
     private function _getCharts($data)
