b18c9a83191d28c61236d67d88ae950a933e208f
Stephen Hamilton <Stephen@CrescentCreative.com>
2021-09-02 13:29:23 -0700

    Working on User Logins Report

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -250,7 +250,8 @@ class ReportsController extends Controller {
 
     private function _dispatchJob(Report $report, Request $request, \Illuminate\Database\Eloquent\Collection $distribution)
     {
-        $manager = request('manager');
+        
+        $manager = $this->_getManager($request);
         $period['from']=Carbon::parse(request('fromdate'))->startOfDay();
         $period['to'] = Carbon::parse(request('todate'))->endOfDay();
    
@@ -262,7 +263,11 @@ class ReportsController extends Controller {
         case 'Campaign':
             return \App\Jobs\CampaignReportJob::dispatch($report, $distribution, $manager)->onQueue('reports');
             break;
-            
+        
+        case 'User':
+            return \App\Jobs\UserReportJob::dispatch($report, $period, $distribution, $manager)->onQueue('reports');
+            break;   
+        
         default:
             return \App\Jobs\BranchReportJob::dispatch($report, $period, $distribution, $manager)->onQueue('reports');
             break; 
@@ -308,6 +313,14 @@ class ReportsController extends Controller {
 
             
         }
+    }
+
+    private function _getManager(Request $request)
+    {
+        if (! request()->filled('manager')) {
+            return $this->person->getCapoDiCapo()->id;
+        }
+        return request('manager');
     }
     /**
      * [_getMyTeam description]
---
+++ app/Jobs/UserReportJob.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Jobs;
+
+use Mail;
+use App\Report;
+use App\Person;
+use App\User;
+use App\Exports\ActivityOpportunityExport;
+
+use Illuminate\Support\Str;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+
+class UserReportJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    
+    public $distribution;
+    public $file;
+    public $manager;
+    public $period;
+    public $person;
+    public $report; 
+    public $user;
+    
+    public function __construct(
+        Report $report, 
+        Array $period = null, 
+        $distribution = null, 
+        $manager = null
+    ) {
+        
+        $this->period = $period;
+        $this->report = $report;
+        $this->manager = $manager;   
+        $this->distribution = $this->report->distribution;
+       
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $distribution = $this->_getDistribution();
+       
+        foreach ($distribution as $recipient) {
+            $this->user = $recipient;
+            $this->file = $this->_makeFileName();
+           
+            $export = $this->_getExportClass();
+            
+            (new $export($this->period, [$this->manager]))
+                ->store($this->file, 'reports')
+                ->chain(
+                    [
+                        new ReportReadyJob(
+                            $recipient, 
+                            $this->period, 
+                            $this->file, 
+                            $this->report
+                        )
+                    ]
+                )->onQueue('reports');
+            
+        }
+    }
+
+    private function _makeFileName()
+    {
+        if (! is_a($this->user, 'App\User')) {
+            $this->user = User::with('person')->first();          
+        }
+
+        return 
+                strtolower(
+                    Str::slug(
+                        $this->report->report." ".
+                        $this->report->filename ." ". 
+                        $this->period['from']->format('Y_m_d'), 
+                        '_'
+                    )
+                ). ".xlsx";  
+        
+    }
+
+    private function _getReportBranches($recipient)
+    {
+        if ($this->manager) {
+
+            return Person::findOrFail($this->manager)->getMyBranches();
+        }
+        return $recipient->person->getMyBranches();
+    }
+
+    private function _getExportClass()
+    {
+        switch(strtolower($this->report->object)) {
+        case "branch":
+            return "\App\Exports\Reports\Branch\\". $this->report->export;
+            break;
+
+        case "company": 
+            return "\App\Exports\\". $this->report->export;
+            break;
+
+
+        
+
+        case "role": 
+            return "\App\Exports\\". $this->report->export;
+            break;
+
+
+        case "campaign": 
+            return "\App\Exports\Campaign\\". $this->report->export;
+            break;
+
+        case "user": 
+            return "\App\Exports\\". $this->report->export;
+            break;
+
+        default:
+            return "\App\Exports\\". $this->report->export;
+            break;
+
+        } 
+
+    }
+    private function _getDistribution()
+    {
+        if (! $this->report->distribution->count()) {
+            return User::where('id', auth()->user()->id)->get();
+        }
+        return $this->report->distribution;
+    }  
+}
