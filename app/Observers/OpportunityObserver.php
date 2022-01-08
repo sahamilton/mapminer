544c8ea38cd71fc3555f478f440500a52dced746
Stephen Hamilton <Stephen@CrescentCreative.com>
2022-01-08 08:04:21 -0800

    Adding is customer to opportunity observer; Removing Erik Lynn branch filter on notification

--- app/Observers/OpportunityObserver.php
+++ app/Observers/OpportunityObserver.php
@@ -4,73 +4,76 @@ namespace App\Observers;
 
 use App\Jobs\WonOpportunity;
 use App\Opportunity;
+use App\Address;
+
 
 class OpportunityObserver
 {
-    // limit to eric lynn's branches
-    public $branches = ['7254',
-             '1678',
-             '1687',
-             '1693',
-             '2666',
-             '1686',
-             '1708',
-             '1677',
-             '1689',
-             '8058',
-             '1684',
-             '1691',
-             '1679',
-             '1676',
-             '1160',
-             '1159',
-             '3300',
-             '1152',
-             '1151',
-             '1161',
-             '2256',
-             '1153',
-             '7386',
-             '1875',
-             '1610',
-             '1611',
-             '3424',
-             '1605',
-             '1878',
-             '1661',
-             '1668',
-             '1669',
-             '7262',
-             '1665',
-             '3062',
-             '3063',
-             '7261',
-             '2681',
-             '3064',
-             '3426',
-             '1115',
-             '2255',
-             '2752',
-             '2961',
-             '2254',
-             '2753',
-             '1150',
-             '2702',
-             '1149',
-             '2751',
-             '2700',
-             '3400',
-             '3403',
-             '3430',
-             '3431',
-             '3433',
-             '8038',
-             '3434',
-             '3418',
-             '3422',
-             '3404',
-             '3411',
-             '3417',
+
+
+    /**
+     * Handle the opportunity "created" event.
+     *
+     * @param  \App\Opportunity  $opportunity
+     * @return void
+     */
+    public function created(Opportunity $opportunity)
+    {
+        //
+    }
+
+    /**
+     * Handle the opportunity "updated" event.
+     *
+     * @param \App\Opportunity $opportunity
+     *
+     * @return void
+     */
+    public function updated(Opportunity $opportunity)
+    {
+        
+        if ($opportunity->closed == 1) {
+             Address::where('id', $opportunity->address_id)->update(['isCustomer' => 1]);
+            if ($opportunity->value > 10000) {
+                WonOpportunity::dispatch($opportunity);
+            }
+        }
+    }
+
+    /**
+     * Handle the opportunity "deleted" event.
+     *
+     * @param  \App\Opportunity  $opportunity
+     * @return void
+     */
+    public function deleted(Opportunity $opportunity)
+    {
+        //
+    }
+
+    /**
+     * Handle the opportunity "restored" event.
+     *
+     * @param  \App\Opportunity  $opportunity
+     * @return void
+     */
+    public function restored(Opportunity $opportunity)
+    {
+        //
+    }
+
+    /**
+     * Handle the opportunity "force deleted" event.
+     *
+     * @param  \App\Opportunity  $opportunity
+     * @return void
+     */
+    public function forceDeleted(Opportunity $opportunity)
+    {
+        //
+    }
+}
+417',
              '2685',
              '2686',
              '2684',
