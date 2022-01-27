3f5501e380895c4e3be6d8dea44dee6201a73523
Stephen Hamilton <Stephen@CrescentCreative.com>
2022-01-27 12:12:14 -0800

    REfactoring Oracle Controller : REmove methods

--- app/Http/Controllers/OracleController.php
+++ app/Http/Controllers/OracleController.php
@@ -3,8 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Oracle;
-use App\Http\Requests\StoreoracleRequest;
-use App\Http\Requests\UpdateoracleRequest;
+
 
 class OracleController extends Controller
 {
@@ -18,26 +17,6 @@ class OracleController extends Controller
         return response()->view('oracle.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreoracleRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreOracleRequest $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
@@ -51,39 +30,6 @@ class OracleController extends Controller
         return response()->view('oracle.show', compact('oracle'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\oracle  $oracle
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(oracle $oracle)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateoracleRequest  $request
-     * @param  \App\oracle  $oracle
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateoracleRequest $request, oracle $oracle)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\oracle  $oracle
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(oracle $oracle)
-    {
-        //
-    }
     /**
      * [unmatched description]
      * @return [type] [description]
