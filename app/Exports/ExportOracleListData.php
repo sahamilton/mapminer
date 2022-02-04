19706b71b973be842332490845a149a386915e8a
Stephen Hamilton <Stephen@CrescentCreative.com>
2022-02-04 11:11:44 -0800

    adding Oracle Exports

---
+++ app/Exports/ExportOracleListData.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Exports;
+use App\User;
+use App\Role;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use \Illuminate\Database\Eloquent\Collection;
+
+class ExportOracleListData implements FromView
+{
+    public $users;
+
+    public function __construct(\Illuminate\Database\Eloquent\Collection $users)
+    {
+        $this->users = $users;
+    }
+
+    
+
+    /**
+     * [view description].
+     *
+     * @return [type] [description]
+     */
+    public function view(): View
+    {
+             
+        return view('oracle.exportOracle', ['users' => $this->users]);
+    }
+}
