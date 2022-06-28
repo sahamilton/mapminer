4d046b99aa4924a8723900acde133eac5f4b989b
Stephen Hamilton <stephen@crescentcreative.com>
2022-06-28 12:08:31 -0700

    Working on Lead specialist roles

--- app/Http/Livewire/AssignLeads.php
+++ app/Http/Livewire/AssignLeads.php
@@ -1,1 +1,69 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use Livewire\WithPagination;
+use App\Location;
+use App\Branch;
+use App\Address;
+class AssignLeads extends Component
+{
+    use WithPagination, NearbyGeocoder;
+    public $perPage = 10;
+    public $sortField = 'distance';
+    public $sortAsc = true; 
+    public $paginationTheme = 'bootstrap';
+    public $distance = '25';
+    public $leaddistance = '0.01';
+    public $address;
+    public Location $location;
+
+    
+    
+    public function sortBy($field)
+    {
+        if ($this->sortField === $field) {
+            $this->sortAsc = ! $this->sortAsc;
+        } else {
+            $this->sortAsc = true;
+        }
+
+        $this->sortField = $field;
+    }
+
+    public function mount()
+    {
+        $this->_geoCodeHomeAddress(); 
+    }
+
+    public function render()
+    {
+        $this->updateAddress();
+        @ray($this->location);
+        return view('livewire.assign-leads',
+            [
+                'branches'=>Branch::query()
+                    ->with('branchteam.reportsto')
+                    
+                    ->nearby($this->location, $this->distance)
+                    ->orderBy($this->sortField, $this->sortAsc ? 'asc' : 'desc')
+                    ->paginate($this->perPage),
+                
+                'leads'=>Address::query()
+                    ->with('assignedToBranch', 'company')
+
+                    ->nearby($this->location, $this->leaddistance)
+                    ->orderBy('distance', 'asc')
+                    ->get(),
+
+
+            ]
+        );
+    }
+
+    
+
+}
+
 
